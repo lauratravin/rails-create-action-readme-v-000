8da8541bde45619be9b6ca60cbb5ba4592608484
Laura Messa Travin <laura.travin@gmail.com>
2021-03-17 21:19:16 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # resources :posts
   resources :posts, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/posts/:id', to: 'posts#show', as: 'post'
 end
